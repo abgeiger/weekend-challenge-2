5fb9bf62e0c3f2886ed64b45f219a9433ec0bd81
abgeiger <30698092+abgeiger@users.noreply.github.com>
2017-11-19 20:49:07 -0600

    completed hard and pro modes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,79 +1,130 @@
 console.log('js sourced');
 var operation = 'none';
+var stage = 'first';
+var firstNumber = '';
+var secondNumber = '';
+var answer = '';
 
 $(document).ready(readyNow);
 
+// event listeners
 function readyNow() {
     console.log('JQ sourced');
-    $('#additionButton').on('click', additionButtonClick);
-    $('#subtractionButton').on('click', subtractionButtonClick);
-    $('#multiplicationButton').on('click', multiplicationButtonClick);
-    $('#divisionButton').on('click', divisionButtonClick);
+    displayHistory();
     $('#postButton').on('click', postButtonClick);
     $('#clearButton').on('click', clearButtonClick);
+    $('.operatorButton').on('click', operatorButtonClick);
+    $('.numberButton').on('click', numberButtonClick);
 }
 
-function additionButtonClick() {
-    operation = '+';
+function numberButtonClick() {
+    if (stage === 'first') {
+        firstNumber += $(this).text();
+        $('.display-screen-span').append('<span>' + $(this).text() + '</span>');
+    } else if (stage === 'second') {
+        secondNumber += $(this).text();
+        $('.display-screen-span').append('<span>' + $(this).text() + '</span>');
+    } else if (stage === 'third') {
+        stage = 'first';
+        $('.display-screen-span').html('');
+        firstNumber += $(this).text();
+        $('.display-screen-span').append('<span>' + $(this).text() + '</span>');
+    }
 }
 
-function subtractionButtonClick() {
-    operation = '-';
-}
-
-function multiplicationButtonClick() {
-    operation = '*';
-}
-
-function divisionButtonClick() {
-    operation = '/';
+function operatorButtonClick() {
+    if (stage === 'first' && firstNumber !== '') {
+        operation = $(this).text();
+        $('.display-screen-span').append('<span> ' + operation + ' </span>');
+        stage = 'second';
+    } else if (stage === 'second' && secondNumber === '') {
+        operation = $(this).text();
+        $('.display-screen-span').children().last().html(' ' + operation + ' ');
+    }
 }
 
 function postButtonClick() {
-    var object = {};
-    var $inputOne = $('#inputOne').val();
-    var $inputTwo = $('#inputTwo').val();
-
-    if ($inputOne !== undefined && $inputTwo !== undefined && operation !== 'none') {
-        object.inputOne = $inputOne;
-        object.inputTwo = $inputTwo;
+    if (stage === 'second' && secondNumber !== '') {
+        var object = {};
+        object.inputOne = firstNumber;
+        object.inputTwo = secondNumber;
         object.operation = operation;
-    } else {
-        console.log('invalid inputs and/or operation');
+
+        // send math problem to server
+        $.ajax({
+            method: 'POST',
+            url: '/calculate',
+            data: object,
+            success: function(response){
+            console.log('object sent to calculate:', object);
+            console.log('response is:', response);
+            },
+            error: function(error){
+            console.log('The "/calculate" ajax post request failed with error: ', error);
+            }
+        });
+
+        // retrieve answer to math problem with get request, run post request to add the answer to the history module, 
+        // and get complete history from history module and put in in the history screen
+        $.ajax({
+            method: 'GET',
+            url: '/calculate',
+            success: function(response) {
+                console.log('the calculated data returned is:', response);
+                answer = response.number;
+                $('.display-screen-span').append('<span> = ' + response.number + '</span>');
+                stage = 'third';
+                operation = 'none';
+                firstNumber = '';
+                secondNumber = '';
+
+                // run post request to add the answer to the history module
+                $.ajax({
+                    method: 'POST',
+                    url: '/calculate/answer',
+                    data: {answer: answer},
+                    success: function(response){
+                        console.log('object sent as answer:', object);
+                        console.log('response is:', response);
+
+                        displayHistory();
+                    },
+                    error: function(error){
+                    console.log('The "/calculate/answer" ajax post request failed with error: ', error);
+                    }
+                });
+            }
+        });
     }
-
-    $.ajax({
-        method: 'POST',
-        url: '/calculate',
-        data: object,
-        success: function(response){
-          console.log('object sent to calculate:', object);
-          console.log('response is:', response);
-        },
-        error: function(error){
-          console.log('The "/calculate" ajax post request failed with error: ', error);
-        }
-      });
-
-      $.ajax({
-        method: 'GET',
-        url: '/calculate',
-        success: function(response) {
-            console.log('the calculated data returned is:', response);
-            $('#answer').html(response.number);
-            
-        }
-    });
-
-    operation = 'none';
-    $('input').val('');
 }
 
+// get complete history from history module and put in in the history screen
+function displayHistory() {
+    $.ajax({
+        method: 'GET',
+        url: '/calculate/history',
+        success: function(response) {
+            console.log('the history array is:', response);
+            var output = '';
+            for (var index = 0; index < response.length; index++) {
+                output += '<p>' + response[index].inputOne + ' ' + response[index].operation + ' ' + response[index].inputTwo + ' = ' + response[index].answer + '</p>';
+            }
+            $('.history-screen').html(output);
+        }
+    });
+}
+
+// resets glabal variables, clears display-screen-span, sets stage to first, and runs a get request to clear
+//  calculateData in routes/calculate.js
 function clearButtonClick() {
     operation = 'none';
-    $('input').val('');
-    $('#answer').html('');
+    firstNumber = '';
+    secondNumber = '';
+    answer = '';
+    $('.display-screen-span').html('');
+    stage = 'first';
 
+    // runs a get request to clear calculateData in routes/calculate.js
     $.ajax({
         method: 'GET',
         url: '/calculate/clear',
--- server/routes/calculate.js
+++ server/routes/calculate.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var router = express.Router();
+var history = require('../modules/history.js');
 
 var calculateData;
+var data_to_store;
 
-router.post('/', function(req,res) {
+// does the math and stores it in calculateData and history
+router.post('/', function(req, res) {
+   history.push(req.body);
     if (req.body.operation === '+') {
         calculateData = Number(req.body.inputOne) + Number(req.body.inputTwo);
     } else if (req.body.operation === '-') {
@@ -17,13 +21,28 @@ router.post('/', function(req,res) {
     res.sendStatus(200);
 });
 
+// sends back calculateData
 router.get('/', function(req, res) {
     res.send({number: calculateData});
 });
 
-router.get('/clear', function(req,res) {
+// clears calculateData
+router.get('/clear', function(req, res) {
     calculateData = undefined;
     res.sendStatus(200);
 });
+
+// sends history back to client
+router.get('/history', function(req, res) {
+    console.log('returning history array:', history);
+    res.send(history);
+});
+
+// adds the answer to the latest object in the history array
+router.post('/answer', function(req, res) {
+    history[history.length - 1].answer = req.body.answer;
+
+    res.sendStatus(200);
+ });
 
 module.exports = router;
